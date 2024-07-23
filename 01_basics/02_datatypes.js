c3e8032916ffcaf71015980b940bfa4f27e3a05f
Mahesh Arjun Wagh <122221998+mahesh3514@users.noreply.github.com>
2024-07-23 11:28:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"Use Strict"; // "" Treat all js code as newer version
+
+// alert(3+3)  //we are using nodejs not browser (works in only browser)
+
+
+let name= "Mahesh"       //under "" or '' it will be string data type
+let age= 25              //without any quot define any number data type
+let isLoggedIn= false    // boolean data type
+let state= null          // Empty object data type+
+
+
+/*
+
+//-------------primitive Data Types------------
+
+Number=> 2 to power 53
+BigInt
+string=> ""  or ''
+boolean => true or false
+null => standalone value 
+undefined => value not yet assigned
+Symbol => Used for unique
+
+*/
+
+
+//object
+
+//--------------find datatypes directly-----------------
+
+console.log(typeof "Mahesh")
+console.log(typeof 12345)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof true)
